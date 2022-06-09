1d59cc4a8bd0816df2400b2b997ecf95a36f45d4
keneversley <krazorfbx@gmail.com>
2022-06-09 00:52:05 -0700

    first

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+     
+  return (
+    <div className='header'>
+        <h1>{title}</h1>
+        <Button 
+        color={showAdd ? 'red' :'green'}
+        text={showAdd ? 'Close' : 'Add'} onClick={onAdd} 
+        />
+        </div>
+  )
+}
+
+Header.defaultProps = {
+    title: 'TODO APP',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+//css in js
+
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+export default Header
